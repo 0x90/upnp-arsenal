8a00ff947f41231d9b2c1ba94b9196ce11cfceda
0x90 <oleg.kupreev@gmail.com>
2017-01-14 22:40:46 +0300

    upnp update

--- miranda-upnp/README.md
+++
@@ -1,4 +0,0 @@
-# miranda-upnp
-Automatically exported from code.google.com/p/miranda-upnp
-
-UPnP command line tool.
--- ufuzz/lib/ufuzz/extensions.rb
+++
@@ -1,9 +0,0 @@
-class String
-  def limit(len)
-    if self.length > len
-      self[0,len]
-    else
-      self
-    end
-  end
-end
